739ac93480a0233543dba6351f3a32b67d605f1b
Mary Sanni <marysanni01@yahoo.com>
2022-11-29 12:20:07 -0800

    first commit

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Mary Sanni
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author -Mary Sanni
+
+def pow(a, b):
+    return (a ** b)
